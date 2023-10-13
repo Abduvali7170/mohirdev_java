d119edd9b1be206b5aec59367891c4f0bf607363
Abduvali7170 <abduvali210984@gmail.com>
2023-10-13 11:23:00 +0500

    Add

--- OOP/Test1.java
+++ OOP/Test1.java
@@ -3,5 +3,6 @@ package OOP;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("Assalamu alaykum");
+        System.out.println("Vaalaykum assalom");
     }
 }
