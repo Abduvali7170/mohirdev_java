d83226a7a358405f40baa603e36112480db9376c
Abduvali7170 <abduvali210984@gmail.com>
2023-10-17 15:20:35 +0500

    some

--- OOP/Test1.java
+++
@@ -1,8 +0,0 @@
-package OOP;
-
-public class Test1 {
-    public static void main(String[] args) {
-        System.out.println("Assalamu alaykum");
-        System.out.println("Vaalaykum assalom");
-    }
-}
