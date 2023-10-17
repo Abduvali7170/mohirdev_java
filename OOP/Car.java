ea0ea500eac3b02ca274f98929cbbf9f6f8f64b7
Abduvali7170 <abduvali210984@gmail.com>
2023-10-17 15:29:17 +0500

    changed

--- OOP/Car.java
+++ OOP/Car.java
@@ -1,2 +1,19 @@
-package OOP;public class Car {
+package OOP;
+
+public class Car {
+    // changed
+    private String color, model, transmission;
+    private Double price = 0.00;
+
+    public Car(){
+        this.color = "GAZ";
+        this.model = "1JX69";
+        this.price = 11200.00;
+        this.transmission = "Manual";
+    }
+    public static void main(String[] args) {
+        Car car = new Car();
+        System.out.println(car.color);
+
+    }
 }
--- OOP/Triangle.java
+++ OOP/Triangle.java
@@ -1,2 +1,17 @@
-package OOP;public class Triangle {
+package OOP;
+
+public class Triangle {
+    // changed
+    int base, height, hypotenuse;
+
+    public Triangle(){
+        this.base = 3;
+        this.height = 4;
+        this.hypotenuse = 5;
+    }
+
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+        System.out.println(triangle.base);
+    }
 }
